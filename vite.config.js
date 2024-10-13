2d00f04752ad7a20e9bf5baf367da0339e59aca3
pankaj priyadershi <pankajpriyadershi@gmail.com>
2024-10-13 23:23:46 +0530

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Digital_Clock/"
+  base: "/digital-clock/"
 })
